52b7260cacfc34972272f0067099581212ef892a
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2022-09-13 19:44:00 +0300

    Update locks.py

--- R0R77/plugins/locks.py
+++ R0R77/plugins/locks.py
@@ -3,56 +3,57 @@ from R0R77 import R0R77
 from R0R77.status import *
 
 LOCKS_HELP = """
-**هذه هي اوامر القفل و الفتح  في الدردشة**
+**ئەمانە فەرمانەکانی قفڵکردن و کردنەوەی قفڵن لە چاتدا**
 
-!قفل
-لقفل شيء معين في الدردشة
+!قوفڵ
+بۆ قفڵکردنی شتێکی تایبەت لە چاتەکەدا
 
-!فتح 
-لفتح الصلاحيات عن شيء معين
+!کردنەوە
+بۆ کردنەوەی مۆڵەتەکان بۆ شتێکی دیاریکراو
 
-الصلاحيات
-لعرض الصلاحيات التي يمكنك قفلها
+/دەسەڵاتەکان
+بۆ پیشاندانی ئەو هێزانەی کە دەتوانیت قفڵی بکەیت
+
 """
 
-@R0R77.on(events.NewMessage(pattern="^[!?/]قفل ?(.*)"))
+@R0R77.on(events.NewMessage(pattern="^[!?/]قوفڵ ?(.*)"))
 @is_admin
 async def lock(event, perm):
     if not perm.change_info:
-      await event.reply("تحتاج الى بعض صلاحيات المشرف لاستخدام هذا الامر")
+      await event.reply("بۆ بەکارهێنانی ئەم فرمانە پێویستت بە هەندێک ئیمتیازاتی بەڕێوەبەر هەیە")
       return
     input_str = event.pattern_match.group(1)
     if not input_str:
-       await event.reply("يرجى تحديد شيء لقفله اولا")
+       await event.reply("تکایە سەرەتا شتێک هەڵبژێرە بۆ ئەوەی قفڵی بکەیت")
        return
-    if "الرسائل" in input_str:
+    if "پەیامەکان" in input_str:
        await R0R77.edit_permissions(event.chat_id, send_messages=False)
-       await event.reply("- تم قفل الرسائل")
-    elif "الميديا" in input_str:
+       await event.reply("- نامەکان قفڵ دەکرێن")
+    elif "ڕاگەیاندن" in input_str:
        await R0R77.edit_permissions(event.chat_id, send_media=False)
-       await event.reply("- تم قفل الوسائط")
-    elif "الملصقات" in input_str:
+       await event.reply("- میدیا قفڵ کراوە")
+    elif "ستیکەرەکان" in input_str:
        await R0R77.edit_permissions(event.chat_id, send_stickers=False)
-       await event.reply("- تم قفل الملصقات.")
-    elif "المتحركة"in input_str:
+       await event.reply("- ستیکەرەکان قفڵ کراون.")
+    elif "گیڤه کان"in input_str:
        await R0R77.edit_permissions(event.chat_id, send_gifs=False)
-       await event.reply("- تم قفل المتحركة")
-    elif "التوجيه" in input_str:
+       await event.reply("- گیڤه کان قفڵ کراوە")
+    elif "ڕێڕەوکردن" in input_str:
        await R0R77.edit_permissions(event.chat_id, forwards=False)
-       await event.reply("- تم قفل التوجيه")
-    elif "الالعاب" in input_str:
+       await event.reply("- ڕێڕەوکردن قفڵ کراوە")
+    elif "یارییەکان" in input_str:
        await R0R77.edit_permissions(event.chat_id, send_games=False)
-       await event.reply("- تم قفل الالعاب")
-    elif "الانلاين" in input_str:
+       await event.reply("- یارییەکان قفڵ دەکرێن")
+    elif "لەسەر هێڵ" in input_str:
        await R0R77.edit_permissions(event.chat_id, send_inline=False)
-       await event.reply("- تم قفل الانلاين")
-    elif "التصويت" in input_str:
+       await event.reply("- ئۆنلاین داخراوە")
+    elif "ده‌نگدان" in input_str:
        await R0R77.edit_permissions(event.chat_id, send_polls=False)
-       await event.reply("- تم قفل التصويت")
-    elif "الروابط" in input_str:
+       await event.reply("- دەنگدان داخراوە")
+    elif "بەستەرەکان" in input_str:
        await R0R77.edit_permissions(event.chat_id, embed_link_previews=False)
-       await event.reply("- تم قفل الروابط")
-    elif "الكل" in input_str:
+       await event.reply("- لینکەکان قفڵ کراون")
+    elif "هه موان" in input_str:
        await R0R77.edit_permissions(event.chat_id,
           send_messages=False, 
           send_media=False,
@@ -62,78 +63,78 @@ async def lock(event, perm):
           send_inline=False,
           send_polls=False,
           embed_link_previews=False)
-       await event.reply("- تم قفل الكل")
+       await event.reply("- هەموویان قفڵ کراون")
 
 
-@R0R77.on(events.NewMessage(pattern="^[!?/]فتح ?(.*)"))
+@R0R77.on(events.NewMessage(pattern="^[!?/]!کردنەوە ?(.*)"))
 @is_admin
 async def unlock(event, perm):
     if not perm.change_info:
-      await event.reply("تحتاج الى بعض صلاحيات المشرف لاستخدام هذا")
+      await event.reply("بۆ بەکارهێنانی ئەمە پێویستت بە هەندێک ئیمتیازاتی بەڕێوەبەر هەیە")
       return
     input_str = event.pattern_match.group(1)
     if not input_str:
-       await event.reply("يرجى تحديد شيء لالغاء قفله اولا")
+       await event.reply("تکایە سەرەتا شتێک هەڵبژێرە بۆ ئەوەی قفڵی بکەیتەوە")
        return
-    if "الرسائل" in input_str:
+    if "پەیامەکان" in input_str:
        await R0R77.edit_permissions(event.chat_id, send_messages=True)
-       await event.reply("تم فتح الكتابة")
-    elif "الميديا" in input_str:
+       await event.reply("نووسین کراوەتەوە")
+    elif "ڕاگەیاندن" in input_str:
        await R0R77.edit_permissions(event.chat_id, send_media=True)
-       await event.reply("تم فتح الوسائط")
-    elif "الملصقات" in input_str:
+       await event.reply("میدیا کرایەوە")
+    elif "ستیکەرەکان" in input_str:
        await R0R77.edit_permissions(event.chat_id, send_stickers=True)
-       await event.reply("تم فتح الملصقات")
-    elif "المتحركة"in input_str:
+       await event.reply("ستیکەرەکان کراوەن")
+    elif "گیڤه کان"in input_str:
        await R0R77.edit_permissions(event.chat_id, send_gifs=True)
-       await event.reply("تم فتح المتحركة")
-    elif "التوجيه" in input_str:
+       await event.reply("گیڤه کان کرایەوە")
+    elif "ڕێڕەوکردن" in input_str:
        await R0R77.edit_permissions(event.chat_id, forwards=True)
-       await event.reply("تم فتح التوجيه")
-    elif "الالعاب" in input_str:
+       await event.reply("ڕێڕەو کراوەتەوە")
+    elif "یارییەکان" in input_str:
        await R0R77.edit_permissions(event.chat_id, send_games=True)
-       await event.reply("تم فتح الالعاب")
-    elif "الانلاين" in input_str:
+       await event.reply("یارییەکان کراوەن")
+    elif "لەسەر هێڵ" in input_str:
        await R0R77.edit_permissions(event.chat_id, send_inline=True)
-       await event.reply("تم فتح الانلاين")
-    elif "التصويت" in input_str:
+       await event.reply("ئۆنلاین کراوەتەوە")
+    elif "ده‌نگدان" in input_str:
        await R0R77.edit_permissions(event.chat_id, send_polls=True)
-       await event.reply("تم فتح `التصويت")
-    elif "الروابط" in input_str:
+       await event.reply("دەنگدان کراوەتەوە")
+    elif "بەستەرەکان" in input_str:
        await R0R77.edit_permissions(event.chat_id, embed_link_previews=True)
-       await event.reply("تم فتح الروابط")
-    elif "الكل" in input_str:
+       await event.reply("لینکەکان کرانەوە")
+    elif "هه موان" in input_str:
        await R0R77.edit_permissions(event.chat_id,
           send_messages=True, 
           send_media=True,
           send_stickers=True,
           send_gifs=True,
           send_games=True,
-          send_inline=True,
+          =True,
           send_polls=True,
           embed_link_previews=True)
-       await event.reply("تم فتح الكل")
+       await event.reply("هەموویان کراوەتەوە")
 
 
-@R0R77.on(events.NewMessage(pattern="^[!?/]الصلاحيات"))
+@R0R77.on(events.NewMessage(pattern="^[!?/]دەسەڵاتەکان"))
 async def locktypes(event):
     TEXT = """
 **Locks:**
 
-‣ الرسائل
-‣ الميديا
-‣ الملصقات
-‣ المتحركة
-‣ الروابط
-‣ الالعاب
-‣ الانلاين
-‣ التوجيه
-‣ التصويت
-‣ الكل
+‣ پەیامەکان
+‣ ڕاگەیاندن
+‣ ستیکەرەکان
+‣ گیڤه کان
+‣ بەستەرەکان
+‣ یارییەکان
+‣ لەسەر هێڵ
+‣ ڕێڕەوکردن
+‣ ده‌نگدان
+‣ هه موان
 """
     await event.reply(TEXT)
 
 @R0R77.on(events.callbackquery.CallbackQuery(data="locks"))
 async def _(event):
 
-    await event.edit(LOCKS_HELP, buttons=[[Button.inline("رجوع", data="help")]])
+    await event.edit(LOCKS_HELP, buttons=[[Button.inline("گه رانه وه", data="help")]])
