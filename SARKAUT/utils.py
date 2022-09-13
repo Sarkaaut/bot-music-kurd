b2a314a9c57b80e6c47f74eea8934eddb1dbfda7
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2022-09-13 07:30:46 +0300

    Update utils.py

--- SARKAUT/utils.py
+++ SARKAUT/utils.py
@@ -4,11 +4,11 @@ import importlib
 from pathlib import Path
 
 def load_plugins(plugin_name):
-    path = Path(f"R0R77/plugins/{plugin_name}.py")
-    name = "R0R77.plugins.{}".format(plugin_name)
+    path = Path(f"SARKAUT/plugins/{plugin_name}.py")
+    name = "SARKAUT.plugins.{}".format(plugin_name)
     spec = importlib.util.spec_from_file_location(name, path)
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["R0R77.plugins." + plugin_name] = load
+    sys.modules["SARKAUT.plugins." + plugin_name] = load
     print("تم تنصيب ملف  " + plugin_name)
