369cea8c0bdcec60ba13622fc6ed0c5997c471a9
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2022-09-13 07:15:02 +0300

    Update admins.py

--- SARKAUT/plugins/admins.py
+++ SARKAUT/plugins/admins.py
@@ -1,23 +1,23 @@
-import JE313P
+import SARKAUT
 from telethon import events, Button
-from JE313P import JE313P
-from JE313P.status import *
+from SARKAUT import SARKAUT
+from SARKAUT.status import *
 from telethon.tl.functions.channels import EditAdminRequest
 from telethon.tl.types import ChatAdminRights
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.functions.messages import ExportChatInviteRequest
 
-@JE313P.on(events.callbackquery.CallbackQuery(data="admin"))
+@SARKAUT.on(events.callbackquery.CallbackQuery(data="admin"))
 async def _(event):
 
     await event.edit(ADMIN_TEXT, buttons=[[Button.inline("« گه رانه وه", data="help")]])
 
-@JE313P.on(events.callbackquery.CallbackQuery(data="play"))
+@SARKAUT.on(events.callbackquery.CallbackQuery(data="play"))
 async def _(event):
 
     await event.edit(PLAY_TEXT, buttons=[[Button.inline("« گه رانه وه", data="help")]])
 
-@JE313P.on(events.NewMessage(pattern="^[!?/]unauth ?(.*)"))
+@SARKAUT.on(events.NewMessage(pattern="^[!?/]unauth ?(.*)"))
 @is_admin
 async def promote(event, perm):
     if event.is_private:
@@ -32,8 +32,8 @@ async def promote(event, perm):
     if not input_str and not user:
         await event.reply("پێویستە وەڵامی بەکارهێنەر بدەیتەوە بۆ ئەوەی باری بکات")
         return
-    sed = await JE313P(GetFullUserRequest(id=user.sender_id or input_str))
-    await JE313P(EditAdminRequest(event.chat_id, user.sender_id or input_str, ChatAdminRights(
+    sed = await SARKAUT(GetFullUserRequest(id=user.sender_id or input_str))
+    await SARKAUT(EditAdminRequest(event.chat_id, user.sender_id or input_str, ChatAdminRights(
                     add_admins=False,
                     invite_users=True,
                     change_info=False,
@@ -47,7 +47,7 @@ async def promote(event, perm):
 
     await event.reply(f"بەکارهێنەرەکە بە سەرکەوتوویی بارکراوە {input_str} in {event.chat.title}")
  
-@JE313P.on(events.NewMessage(pattern="^[!?/]unauth ?(.*)"))
+@SARKAUT.on(events.NewMessage(pattern="^[!?/]unauth ?(.*)"))
 @is_admin
 async def promote(event, perm):
     if event.is_private:
@@ -61,8 +61,8 @@ async def promote(event, perm):
     if not input_str and not user:
         await event.reply("پێویستە وەڵامی ئەو بەکارهێنەرە بدەیتەوە کە دەتەوێت دایبەزێنیت")
         return
-    sed = await JE313P(GetFullUserRequest(id=user.sender_id or input_str))
-    await JE313P(EditAdminRequest(event.chat_id, user.sender_id or input_str, ChatAdminRights(
+    sed = await SARKAUT(GetFullUserRequest(id=user.sender_id or input_str))
+    await SARKAUT(EditAdminRequest(event.chat_id, user.sender_id or input_str, ChatAdminRights(
                     add_admins=False,
                     invite_users=None,
                     change_info=None,
@@ -77,13 +77,13 @@ async def promote(event, perm):
     await event.reply(f"- بە سەرکەوتوویی دابەزێنراوە {input_str} in {event.chat.title}")
  
 
-@JE313P.on(events.NewMessage(pattern="^[!?/]link"))
+@SARKAUT.on(events.NewMessage(pattern="^[!?/]link"))
 async def invitelink(event):
 
     if event.is_private:
        await event.reply("ئەم فرمانە تەنها لە گروپدا بەکاردێت !")
        return
-    link = await JE313P(ExportChatInviteRequest(event.chat_id))
+    link = await SARKAUT(ExportChatInviteRequest(event.chat_id))
     await event.reply(f"گروپەکە {event.chat.title}لینک: [کرتەی ئێرە بکە]({link.link})", link_preview=False)
 
 ADMIN_TEXT = """
