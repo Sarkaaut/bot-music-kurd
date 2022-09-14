57125154c5d212bf80562cd37a0a0bd6e87e859a
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2022-09-14 05:44:02 +0300

    Update pins.py

--- R0R77/plugins/pins.py
+++ R0R77/plugins/pins.py
@@ -6,40 +6,39 @@ from R0R77 import *
 
 
 PINS_TEXT = """
-**✘ اوامر تثبيت والغاء التثبيت لرسائل في المجموعة**
+**🔰فەرمانەکانی دامەزراندن و لابردنی نامەکان لە گروپەکەدا**
 
-‣ `!تثبيت`
-بالرد على الرسالة التي تريد تثبيتها
+/Installations
+🖇️ریپله ی ئەو نامەیە بکه کە دەتەوێت دایبمەزرێنیت
 
-‣ `!الغاء تثبيت`
-بالرد على الرسالت التي تريد الغاء تثبيتها
+/uninstall
+📨ریپله ی ئەو نامانەی کە دەتەوێت پینیان لێ بکەیتەوە
 
-‣ `!الغاء التثبيت للكل`
-لالغاء تثبيبت جميع الرسائل في المجموعة
+/Uninstall for all
+📭بۆ لابردنی هەموو نامە پین کراوه کانی ناو گروپەکە
 
-‣ `!الرسائل المثبتة`
-لأظهار الرسائل المثبتة في المجموعة
+/Pinned messages
+📮بۆ پیشاندانی نامە پین کراوەکان لە گروپەکەدا
 
-@jmthon
 """
 
-@R0R77.on(events.NewMessage(pattern="^[?!/]الرسائل المثبتة"))
+@R0R77.on(events.NewMessage(pattern="^[?!/]Pinned messages"))
 async def get_pinned(event):
     chat_id = (str(event.chat_id)).replace("-100", "")
 
     Ok = await R0R77.get_messages(event.chat_id, ids=types.InputMessagePinned()) 
-    tem = f"الرسائل المثبتة في الدردشة{event.chat.title} هي <a href=https://t.me/c/{chat_id}/{Ok.id}>here</a>."
+    tem = f"نامەی پینکراو لە چاتدا{event.chat.title} ئه و <a href=https://t.me/c/{chat_id}/{Ok.id}>here</a>."
     await event.reply(tem, parse_mode="html", link_preview=False)
 
-@R0R77.on(events.NewMessage(pattern="^[!?/]تثبيت ?(.*)"))
+@R0R77.on(events.NewMessage(pattern="^[!?/]Installations ?(.*)"))
 @is_admin
 async def pin(event, perm):
     if not perm.pin_messages:
-       await event.reply("يجب ان تمتلك صلاحيات التثببيت اولا")
+       await event.reply("پێویستە سەرەتا مۆڵەتی دامەزراندنت هەبێت🔰")
        return
     msg = await event.get_reply_message()
     if not msg:
-       await event.reply("يجب عليك الرد على الرسالة اولا")
+       await event.reply("پێویستە سەرەتا ریپله ی نامەکە بکه یت🖇️")
        return
     input_str = event.pattern_match.group(1)
     if "notify" in input_str:
@@ -47,53 +46,53 @@ async def pin(event, perm):
        return
     await R0R77.pin_message(event.chat_id, msg)   
 
-@R0R77.on(events.NewMessage(pattern="^[!?/]الغاء تثبيت ?(.*)"))
+@R0R77.on(events.NewMessage(pattern="^[!?/]uninstall ?(.*)"))
 @is_admin
 async def unpin(event, perm):
     if not perm.pin_messages:
-       await event.reply("يجب ان تمتلك صلاحيات التثببيت اولا")
+       await event.reply("پێویستە سەرەتا مۆڵەتی دامەزراندنت هەبێت🔰")
        return
     chat_id = (str(event.chat_id)).replace("-100", "")
     ok = await R0R77.get_messages(event.chat_id, ids=types.InputMessagePinned())
     await R0R77.unpin_message(event.chat_id, ok)
-    await event.reply(f"تم بنجاح الغاء التثبيت [لهذه الرسالة](t.me/{event.chat.username}/{ok.id}).", link_preview=False)
+    await event.reply(f"دامەزراندنەکە بە سەرکەوتوویی هەڵوەشایەوە✅   [بۆ ئەم پەیامە](t.me/{event.chat.username}/{ok.id}).", link_preview=False)
 
 
-@R0R77.on(events.NewMessage(pattern="^[!?/]الغاء التثبيت للكل$"))
+@R0R77.on(events.NewMessage(pattern="^[!?/]Uninstall for all$"))
 async def unpinall(event, perm):
     if not perm.pin_messages:
-       await event.reply("يجب ان تمتلك صلاحيات التثببيت اولا")
+       await event.reply("پێویستە سەرەتا مۆڵەتی دامەزراندنت هەبێت🔰")
        return
     UNPINALL = """
-هل انت متأكد من الغاء تثبيت الرسائل ؟
+ئایا دڵنیای کە نامەکان هەڵدەگریت؟
 """
 
     await R0R77.send_message(event.chat_id, UNPINALL, buttons=[
-    [Button.inline("تأكيد", data="unpin")], 
-    [Button.inline("الغاء", data="cancel")]])
+    [Button.inline("دووپات کردنەوە", data="unpin")], 
+    [Button.inline("هەڵوەشاندنەوە", data="cancel")]])
 
 @R0R77.on(events.callbackquery.CallbackQuery(data="unpin"))
 async def confirm(event):
     check = await event.client.get_permissions(event.chat_id, event.sender_id)
     if check.is_creator:
         await R0R77.unpin_message(event.chat_id)
-        await event.edit("تم بنجاح الغاء تثببيت جميع الرسائل")
+        await event.edit("هەموو نامەکان بە سەرکەوتوویی لابراون✅")
         return 
 
-    await event.answer("يجب ان تكون مالك المجموعة اولا")
+    await event.answer("پێویستە سەرەتا خاوەنی گروپەکە بیت🔱")
 
 @R0R77.on(events.callbackquery.CallbackQuery(data="cancel"))
 async def cancel(event):
 
     check = await event.client.get_permissions(event.chat_id, event.sender_id)
     if check.is_creator:
-        await event.edit("عملية الغاء تثبيت جميع الرسائل تم الغائها ")
+        await event.edit("پرۆسەی لابردنی دامەزراندن بۆ هەموو نامەکان هەڵوەشاوەتەوە‼️")
         return 
 
-    await event.answer("يجب ان تكون مالك المجموعة اولا")
+    await event.answer("پێویستە سەرەتا خاوەنی گروپەکە بیت🔱")
 
 
 @R0R77.on(events.callbackquery.CallbackQuery(data="pins"))
 async def _(event):
 
-    await event.edit(PINS_TEXT, buttons=[[Button.inline("رجوع", data="help")]])
+    await event.edit(PINS_TEXT, buttons=[[Button.inline("گه رانه وه🔙", data="help")]])
