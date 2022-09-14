5d32c229f756bb697cd68216f067ea7064274a61
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2022-09-14 06:40:59 +0300

    Update play.py

--- R0R77/plugins/play.py
+++ R0R77/plugins/play.py
@@ -125,12 +125,12 @@ async def _(event):
      await event.delete()
 
 btnn =[
-    [Button.url("الدعم", url=f"t.me/{Config.SUPPORT}"), Button.url("القناة", url=f"t.me/{Config.CHANNEL}")],
-    [Button.inline("اغلاق", data="cls")]]
+    [Button.url("پشتگیری", url=f"t.me/{Config.SUPPORT}"), Button.url("که ناڵ", url=f"t.me/{Config.CHANNEL}")],
+    [Button.inline("داخستن", data="cls")]]
 
 
 #play
-@R0R77.on(events.NewMessage(pattern="^[?!/]تشغيل"))
+@R0R77.on(events.NewMessage(pattern="^[?!/]play"))
 async def play(event):
     title = ' '.join(event.text[5:])
     replied = await event.get_reply_message()
@@ -147,14 +147,14 @@ async def play(event):
         or not replied
         and not title
     ):
-        return await event.client.send_file(chat_id, Config.CMD_IMG, caption="**يجب عليك كتابة عنوان الشيء الذي تريد تشغيله**\n\n **مثال**: `!تشغيل سورة الكهف`", buttons=btnn)
+        return await event.client.send_file(chat_id, Config.CMD_IMG, caption="**دەبێت ناونیشانی ئەو شتە بنووسیت کە دەتەوێت جێبەجێی بکەیت✏️**\n\n **نموونە**: `!play سورة الكهف`", buttons=btnn)
     elif replied and not replied.audio and not replied.voice or not replied:
-        botman = await event.reply("يتم التعرف على البيانات انتظر . . .")
+        botman = await event.reply("داتاکان دەناسرێنەوە چاوەڕوان بە 🔍")
         query = event.text.split(maxsplit=1)[1]
         search = ytsearch(query)
         if search == 0:
             await botman.edit(
-                "**- لم يتم العثور على المطلوب اكتب عنوان اخر بشكل اصح**"
+                "**- ⚠️ناونیشانی داواکراو نەدۆزرایەوە ناونیشانێکی تر بە دروستی بنووسە**"
             )     
         else:
             songname = search[0]
@@ -172,7 +172,7 @@ async def play(event):
                 await botman.edit(f"`{ytlink}`")
             elif chat_id in QUEUE:
                 pos = add_to_queue(chat_id, songname, ytlink, url, "Audio", 0)
-                caption = f"- **ت مالاضافة الى قائمة التشغيل»** `#{pos}`\n\n**🏷 العنوان:** [{songname}]({url})\n**⏱ المدة:** `{duration}`\n🎧 **الطب الى:** {from_user}"
+                caption = f"- **زیاد بکە بۆ پلەی لیست»** `#{pos}`\n\n**🏷 ناونیشانەکە:** [{songname}]({url})\n**⏱ ماوه که ی:** `{duration}`\n🎧 **له سه ر داواکاری:** {from_user}"
                 await botman.delete()
                 await event.client.send_file(chat_id, thumb, caption=caption, buttons=btnn)
             else:
@@ -185,7 +185,7 @@ async def play(event):
                         stream_type=StreamType().pulse_stream,
                     )
                     add_to_queue(chat_id, songname, ytlink, url, "Audio", 0)
-                    caption = f"🏷 **العنوان:** [{songname}]({url})\n**⏱ المدة:** `{duration}`\n💡 **الحالة:** شغالة الان\n🎧 **الطب الى:** {from_user}"
+                    caption = f"🏷 **ناونیشانه که:** [{songname}]({url})\n**⏱ ماوه که ی:** `{duration}`\n💡 **بارودۆخەکە:**  ئێستا کار دەکات\n🎧 **له سه ر داواکاری:** {from_user}"
                     await botman.delete()
                     await event.client.send_file(chat_id, thumb, caption=caption, buttons=btnn)
                 except Exception as ep:
@@ -193,7 +193,7 @@ async def play(event):
                     await botman.edit(f"`{ep}`")
 
     else:
-        botman = await event.edit("📥 **جار التحميل**")
+        botman = await event.edit("📥 **له دابه زاندن دایه**")
         dl = await replied.download_media()
         link = f"https://t.me/c/{chat.id}/{event.reply_to_msg_id}"
         if replied.audio:
@@ -202,7 +202,7 @@ async def play(event):
             songname = "Voice Note"
         if chat_id in QUEUE:
             pos = add_to_queue(chat_id, songname, dl, link, "Audio", 0)
-            caption = f"💡 ** تم الاضافة الى قائمة التشغيل**`#{pos}`\n\n**🏷 العنوان:** [{songname}]({link})\n**ايدي الدردشة**: `{chat_id}`\n🎧 **الطلب الى**: {from_user}"
+            caption = f"💡 **زیاد بکە بۆ پلەی لیست➕**`#{pos}`\n\n**🏷 ناونیشانه که:** [{songname}]({link})\n**ئایدی گرووپ**: `{chat_id}`\n🎧 **له سه ر داواکاری**: {from_user}"
             await event.client.send_file(chat_id, ngantri, caption=caption, buttons=btnn)
             await botman.delete()
         else:
@@ -215,7 +215,7 @@ async def play(event):
                     stream_type=StreamType().pulse_stream,
                 )
                 add_to_queue(chat_id, songname, dl, link, "Audio", 0)
-                caption = f"🏷 ** العنوان** [{songname}]({link})\n**ايدي الدردشة**: `{chat_id}`\n💡 **الحالة:** شغالة الان\n🎧 **الطلب الى**: {from_user}"
+                caption = f"🏷 **ناونیشانه که** [{songname}]({link})\n**ئایدی گرووپ**: `{chat_id}`\n💡 **بارودۆخەکە:** ئێستا کار دەکات \n🎧 **له سه ر داواکاری**: {from_user}"
                 await event.client.send_file(chat_id, fotoplay, caption=caption, buttons=btnn)
                 await botman.delete()
             except Exception as ep:
@@ -227,7 +227,7 @@ async def play(event):
 
 
 #end
-@R0R77.on(events.NewMessage(pattern="^[/?!]انهاء"))
+@R0R77.on(events.NewMessage(pattern="^[/?!]pause"))
 @is_admin
 async def vc_end(event, perm):
     chat_id = event.chat_id
@@ -235,20 +235,20 @@ async def vc_end(event, perm):
         try:
             await call_py.leave_group_call(chat_id)
             clear_queue(chat_id)
-            await event.reply("**تم الغاء التشغيل بنجاح**")
+            await event.reply("**بە سەرکەوتوویی وه ستینرا✅**")
         except Exception as e:
-            await event.reply(f"**خطأ:** `{e}`")
+            await event.reply(f"**هەڵە:** `{e}`")
     else:
-        await event.reply("**تم الغاء التشغيل بنجاح**")
+        await event.reply("**بە سەرکەوتوویی وه ستینرا✅**")
 
 
 
 
 
-@R0R77.on(events.NewMessage(pattern="^[?!/]فيديو"))
+@R0R77.on(events.NewMessage(pattern="^[?!/]vplay"))
 async def vplay(event):
     if Config.HEROKU_MODE == "ENABLE":
-        await event.reply("- لا يمكنك استخدام هذا الامر لانك تستخدم هيروكو في التنصيب")
+        await event.reply("- ناتوانیت ئەم فرمانە بەکاربهێنیت چونکە لە ڕێکخستنەکەدا Heroku بەکاردەهێنیت")
         return
     title = ' '.join(event.text[6:])
     replied = await event.get_reply_message()
@@ -267,16 +267,16 @@ async def vplay(event):
         or not replied
         and not title
     ):
-        return await event.client.send_file(chat_id, Config.CMD_IMG, caption="**يجب عليك كتابة عنوان لتشغيله**\n\n **مثال**: `!فيديو قران`", buttons=btnn)
+        return await event.client.send_file(chat_id, Config.CMD_IMG, caption="**بۆ ئەوەی یاری پێبکەیت دەبێت ناونیشانێک بنووسیت**\n\n **نموونه**: `!vplay قران`", buttons=btnn)
     if replied and not replied.video and not replied.document:
-        razan = await event.reply("انتظر قليلا يتم التعرف")
+        razan = await event.reply("کەمێک چاوەڕێ بکە تا بتناسرێتەوە🔍")
         query = event.text.split(maxsplit=1)[1]
         search = ytsearch(query)
         RESOLUSI = 720
         hmmm = HighQualityVideo()
         if search == 0:
             await razan.edit(
-                "**يجب عليك كتابة عنوان صحيح**"
+                "**پێویستە ناونیشانێکی دروست دابنێیت**"
             )
         else:
             query = event.text.split(maxsplit=1)[1]
@@ -295,7 +295,7 @@ async def vplay(event):
             elif chat_id in QUEUE:
                 pos = add_to_queue(
                     chat_id, songname, ytlink, url, "Video", RESOLUSI)
-                caption = f"💡 **تم الاضافة الى قائمة التشغيل** `#{pos}`\n\n**🏷 العنوان:** [{songname}]({url})\n**⏱ المدة** `{duration}`\n🎧 **بطلب من ** {from_user}"
+                caption = f"💡 **زیاد بکە بۆ پلەی لیست➕** `#{pos}`\n\n**🏷 ناونیشانه که:** [{songname}]({url})\n**⏱ ماوه که ی** `{duration}`\n🎧 **لەسەر داواکاری ** {from_user}"
                 await razan.delete()
                 await event.client.send_file(chat_id, thumb, caption=caption, buttons=btnn)
             else:
@@ -314,7 +314,7 @@ async def vplay(event):
                         RESOLUSI)
                     await razan.delete()
                     await event.client.send_file(event.chat_id,
-                        f"**🏷 **تم بدأ تشغيل**:** [{songname}]({url})\n**⏱ المدة** `{duration}`\n💡 **الحالة:** شغالة الان\n🎧 **بطلب من ** {from_user}, buttons=btnn",
+                        f"**🏷 **دەستی پێکردووە**:** [{songname}]({url})\n**⏱ ماوه که ی** `{duration}`\n💡 **بارودۆخەکە:**  ئێستا کار دەکات\n🎧 **لەسەر داواکاری ** {from_user}, buttons=btnn",
                         link_preview=False,
                     )
                 except Exception as ep:
@@ -322,7 +322,7 @@ async def vplay(event):
                     await razan.edit(f"`{ep}`")
 
     elif replied:
-        razan = await event.reply("📥 **يتم تحميل الفيديو انتظر قليلا**")
+        razan = await event.reply("📥 **ڤیدیۆکە باردەبێت، ساتێک چاوەڕێ بکە**")
         dl = await replied.download_media()
         link = f"https://t.me/c/{chat.id}/{event.reply_to_msg_id}"
         if len(event.text.split()) < 2:
@@ -334,7 +334,7 @@ async def vplay(event):
             songname = "Telegram Video Player"
         if chat_id in QUEUE:
             pos = add_to_queue(chat_id, songname, dl, link, "Video", RESOLUSI)
-            caption = f"💡 **تم تشغيل الفيديو في الدردشة** `#{pos}`\n\n**🏷 العنوان:** [{songname}]({link})\n**ايدي الدردشة**: `{chat_id}`\n🎧 **بطلب من ** {from_user}"
+            caption = f"💡 **ڤیدیۆکە لە چاتەکەدا پەخش کرا** `#{pos}`\n\n**🏷 ناونیشانه که:** [{songname}]({link})\n**ئایدی گرووپ**: `{chat_id}`\n🎧 **لەسەر داواکاری ** {from_user}"
             await event.client.send_file(chat_id, ngantri, caption=caption, buttons=btnn)
             await razan.delete()
         else:
@@ -351,20 +351,20 @@ async def vplay(event):
                     stream_type=StreamType().pulse_stream,
                 )
                 add_to_queue(chat_id, songname, dl, link, "Video", RESOLUSI)
-                caption = f"🏷 ** العنوان** [{songname}]({link})\n**ايدي الدردشة**: `{chat_id}`\n💡 **الحالة:** شغالة الان\n🎧 **بطلب من ** {from_user}"
+                caption = f"🏷 **ناونیشانه که** [{songname}]({link})\n**ئایدی گرووپ**: `{chat_id}`\n💡 **بارودۆخەکە:** ئێستا کار دەکات \n🎧 **لەسەر داواکاری ** {from_user}"
                 await razan.delete()
                 await event.client.send_file(chat_id, fotoplay, caption=caption, buttons=btnn)
             except Exception as ep:
                 clear_queue(chat_id)
                 await razan.edit(f"`{ep}`")
     else:
-        razan = await event.reply("- جار البحث انتظر قليلا ")
+        razan = await event.reply("- گەڕان یەک خولەک چاوەڕێ بکە🔍 ")
         query = event.text.split(maxsplit=1)[1]
         search = ytsearch(query)
         RESOLUSI = 720
         hmmm = HighQualityVideo()
         if search == 0:
-            await razan.edit("**لم يتم التعرف على العنوان**")
+            await razan.edit("**ناونیشان نەناسراوەتەوە**")
         else:
             songname = search[0]
             title = search[0]
@@ -380,7 +380,7 @@ async def vplay(event):
             elif chat_id in QUEUE:
                 pos = add_to_queue(
                     chat_id, songname, ytlink, url, "Video", RESOLUSI)
-                caption = f"💡 **تمت الاضافة ال قائمة التشغيل** `#{pos}`\n\n🏷 ** العنوان** [{songname}]({url})\n**⏱ المدة** `{duration}`\n🎧 **بطلب من ** {from_user}"
+                caption = f"💡 **زیاد بکە بۆ پلەی لیست** `#{pos}`\n\n🏷 **ناونیشانه که** [{songname}]({url})\n**⏱ ماوه که ی** `{duration}`\n🎧 **لەسەر داواکاری ** {from_user}"
                 await razan.delete()
                 await event.client.send_file(chat_id, thumb, caption=caption, buttons=btnn)
             else:
@@ -397,7 +397,7 @@ async def vplay(event):
                         url,
                         "Video",
                         RESOLUSI)
-                    caption = f"🏷 ** العنوان** [{songname}]({url})\n**⏱ المدة** `{duration}`\n💡 **الحالة:** شغالة الان\n🎧 **بطلب من ** {from_user}"
+                    caption = f"🏷 **ناونیشانه که** [{songname}]({url})\n**⏱ ماوه که ی** `{duration}`\n💡 **بارودۆخەکە:** ئێستا کار دەکات \n🎧 **لەسەر داواکاری ** {from_user}"
                     await razan.delete()
                     await event.client.send_file(chat_id, thumb, caption=caption, buttons=btnn)
                 except Exception as ep:
@@ -408,7 +408,7 @@ async def vplay(event):
 
 
 #playlist
-@R0R77.on(events.NewMessage(pattern="^[?!/]التشغيل"))
+@R0R77.on(events.NewMessage(pattern="^[?!/]seek"))
 @is_admin
 async def vc_playlist(event, perm):
     chat_id = event.chat_id
@@ -416,11 +416,11 @@ async def vc_playlist(event, perm):
         chat_queue = get_queue(chat_id)
         if len(chat_queue) == 1:
             await event.reply(
-                f"**قائمة التشغيل :**\n• [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}`",
+                f"**پلەی لیست📋 :**\n• [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}`",
                 link_preview=False,
             )
         else:
-            PLAYLIST = f"**🎧 قائمة التشغيل:**\n**• [{chat_queue[0][0]}]({chat_queue[0][2]})** | `{chat_queue[0][3]}` \n\n**• المقاطع المنتظرة:**"
+            PLAYLIST = f"**🎧 پلەی لیست:**\n**• [{chat_queue[0][0]}]({chat_queue[0][2]})** | `{chat_queue[0][3]}` \n\n**• کلیپە چاوەڕوانکراوەکان📽️:**"
             l = len(chat_queue)
             for x in range(1, l):
                 hmm = chat_queue[x][0]
@@ -430,7 +430,7 @@ async def vc_playlist(event, perm):
                     f"**#{x}** - [{hmm}]({hmmm}) | `{hmmmm}`"
             await event.reply(PLAYLIST, link_preview=False)
     else:
-        await event.reply("**لم يتم تشغيل شيء اصلا**")
+        await event.reply("**هیچ شتێک کار ناکات⚠️**")
 
 
 
@@ -438,10 +438,10 @@ async def vc_playlist(event, perm):
 
 
 #كود المغادرة
-@R0R77.on(events.NewMessage(pattern="^[?!/]مغادرة"))
+@R0R77.on(events.NewMessage(pattern="^[?!/]shuffle"))
 @is_admin
 async def leavevc(event, perm):
-    razan = await event.reply("- يرجى الانتظار قليلا")
+    razan = await event.reply("- تکایە کەمێک چاوەڕێ بکە🔄")
     chat_id = event.chat_id
     from_user = vcmention(event.sender)
     if from_user:
@@ -449,30 +449,30 @@ async def leavevc(event, perm):
             await call_py.leave_group_call(chat_id)
         except (NotInGroupCallError, NoActiveGroupCall):
             pass
-        await razan.edit("**- تم مغادرة المكالمة بنجاح للدردشة** `{}`".format(str(event.chat_id)))
+        await razan.edit("**- پەیوەندییەکە بە سەرکەوتوویی جێهێڵراوە بۆ چات✅** `{}`".format(str(event.chat_id)))
     else:
-        await razan.edit(f"**عذرا {owner} يستخدم الامر في الدردشات الصوتية فقط**")
+        await razan.edit(f"**ببوره {owner} تەنها لە چاتی دەنگیدا بەکاردێت🔉**")
 
 
 
-@R0R77.on(events.NewMessage(pattern="^[?!/]تخطي"))
+@R0R77.on(events.NewMessage(pattern="^[?!/]skip"))
 @is_admin
 async def vc_skip(event, perm):
     chat_id = event.chat_id
     if len(event.text.split()) < 2:
         op = await skip_current_song(chat_id)
         if op == 0:
-            await event.reply("- لم يتم تشغيل شيء اصلا")
+            await event.reply("- هیچ شتێک کار ناکات⚠️")
         elif op == 1:
-            await event.reply("القائمة انتهت لذلك غادرت المكالمة, 10")
+            await event.reply("لیستەکە بەسەرچووە بۆیە پەیوەندیەکەم بەجێهێشت⚠️")
         else:
             await event.reply(
-                f"**⏭ تم التخطي**\n**🎧 يتم تشغيل الان** - [{op[0]}]({op[1]})",
+                f"**⏭ پەڕیووەتەوە**\n**🎧 ئێستا کاردەکات** - [{op[0]}]({op[1]})",
                 link_preview=False,
             )
     else:
         skip = event.text.split(maxsplit=1)[1]
-        DELQUE = "**يتم حذف الباقي من قائمة التشغيل:**"
+        DELQUE = "**باقیەکەی تر لە پلەی لیستەکە لادەبرێن:**"
         if chat_id in QUEUE:
             items = [int(x) for x in skip.split(" ") if x.isdigit()]
             items.sort(reverse=True)
@@ -484,33 +484,33 @@ async def vc_skip(event, perm):
             await event.reply(DELQUE)
 
 
-@R0R77.on(events.NewMessage(pattern="^[?!/]ايقاف"))
+@R0R77.on(events.NewMessage(pattern="^[?!/]stop"))
 @is_admin
 async def vc_pause(event, perm):
     chat_id = event.chat_id
     if chat_id in QUEUE:
         try:
             await call_py.pause_stream(chat_id)
-            await event.reply("**تم أيقاف التشغيل مؤقتا**")
+            await event.reply("**پەخشکردنەکە وەستاوە❗**")
         except Exception as e:
-            await event.reply(f"**خطأ** `{e}`")
+            await event.reply(f"**هه ڵه** `{e}`")
     else:
-        await event.reply("**لم يتم تشغيل شيء اصلا**")
+        await event.reply("**هیچ شتێک کار ناکات⚠️**")
 
 
 
-@R0R77.on(events.NewMessage(pattern="^[?!/]استئناف"))
+@R0R77.on(events.NewMessage(pattern="^[?!/]resume"))
 @is_admin
 async def vc_resume(event, perm):
     chat_id = event.chat_id
     if chat_id in QUEUE:
         try:
             await call_py.resume_stream(chat_id)
-            await event.reply(event, "**- تم استئناف التشغيل**")
+            await event.reply(event, "**- پەخشکردن دەستی پێکردەوە✅**")
         except Exception as e:
-            await event.reply(event, f"**خطأ** `{e}`")
+            await event.reply(event, f"**هه ڵه** `{e}`")
     else:
-        await event.reply(event, "**لم يتم تشغيل شيء اصلا**")
+        await event.reply(event, "**هیچ شتێک کار ناکات⚠️**")
 
 
 @call_py.on_stream_end()
