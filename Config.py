a39f351e5cc57c9a71b66976c196b41f790e7914
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2022-09-16 05:43:13 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
     API_ID = int(os.environ.get("APP_ID", "6435225"))
     API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5747867296:AAHqmPf2sVOW0uDvSm6VCmZNh8D4Whk1ttI")
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "JMTHON_ROBOT")
