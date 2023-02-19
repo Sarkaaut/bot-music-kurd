1ba2e6e69ec9d092260efb379724bac716b59975
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2023-02-19 13:04:06 +0300

    Update start.py

--- R0R77/plugins/start.py
+++ R0R77/plugins/start.py
@@ -37,8 +37,8 @@ async def start(event):
 async def _(event):
     if event.is_private:
        await event.edit(PM_START_TEXT.format(event.sender.first_name), buttons=[
-        [Button.url("➕ بۆ ئادم کلیک لێرە بکە", f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+        [Button.url("➕ بۆ ئادم کلیک لێرە بکە ➕", f"https://t.me/{BOT_USERNAME}?startgroup=true")],
         [Button.url("خاوه ن بۆت", "https://t.me/SARKAUT")],
-        [Button.url("پشتگتری", f"https://t.me/{Config.SUPPORT}"), Button.url("که ناڵ", f"https://t.me/{Config.CHANNEL}")],
+        [Button.url("پشگیری", f"https://t.me/{Config.SUPPORT}"), Button.url("که ناڵ", f"https://t.me/{Config.CHANNEL}")],
         [Button.inline("فه رمانه کان", data="help")]])
        return
