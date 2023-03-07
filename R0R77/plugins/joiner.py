985f5480bffc5effe963afa92fdd12d5fab2d360
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2023-03-07 23:56:30 +0300

    Update joiner.py

--- R0R77/plugins/joiner.py
+++ R0R77/plugins/joiner.py
@@ -8,18 +8,6 @@ from telethon.errors.rpcerrorlist import (
     UserAlreadyParticipantError,
     UserNotParticipantError
 )
-from telethon.tl.types import PeerChannel,InputChannel
-from telethon.tl.functions.channels import *
-from telethon.tl.functions.channels import GetParticipantsRequest
-from telethon.tl.types import ChannelParticipantsSearch
-import telethon
-from telethon.tl import functions
-from telethon.tl.functions.channels import LeaveChannelRequest
-from telethon.tl.functions.messages import ImportChatInviteRequest
-from telethon.tl.functions.channels import LeaveChannelRequest
-from telethon.tl.functions.messages import ImportChatInviteRequest
-from telethon.tl.functions.messages import ExportChatInviteRequest
-
 
 def AssistantAdd(mystic):
     async def wrapper(event):
