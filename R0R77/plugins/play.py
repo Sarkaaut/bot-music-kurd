817888131e5ffc1a1b611e9ccb4d56526f52a01c
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2023-03-18 22:09:00 +0300

    Update play.py

--- R0R77/plugins/play.py
+++ R0R77/plugins/play.py
@@ -28,7 +28,7 @@ from youtubesearchpython import VideosSearch
 fotoplay = "https://telegra.ph/file/b6402152be44d90836339.jpg"
 ngantri = "https://telegra.ph/file/b6402152be44d90836339.jpg"
 from R0R77 import call_py, R0R77, client as Client
-owner = "1669178360"
+owner = "5789819429"
 from R0R77.helpers.yt_dlp import bash
 from R0R77.helpers.chattitle import CHAT_TITLE
 from R0R77.helpers.queues import (
